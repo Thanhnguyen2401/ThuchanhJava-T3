779b5be507d808defea3e71b5a633f014a956e20
Thành Nguyễn <66818327+Thanhnguyen2401@users.noreply.github.com>
2021-04-17 22:46:41 +0700

    Add files via upload

---
+++ lab04/src/main/java/com/mycompany/lab04/jPopupMenu1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab04;
+
+/**
+ *
+ * @author Admin
+ */
+class jPopupMenu1 {
+
+    static void show(bai5 aThis, int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
